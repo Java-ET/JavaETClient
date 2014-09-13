8f0957ee2f9bedaa89a535f24fa1d6e5c26b1156
Ziggeh <ziggy@projectswg.com>
2014-09-14 00:41:04 +0200

    Fixed an issue with the PAK3 utility

--- src/utilities/PAK3.java
+++ src/utilities/PAK3.java
@@ -2,7 +2,6 @@ package utilities;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -11,14 +10,14 @@ import main.ETConsole;
 
 public class PAK3 {
 	
-	public static ArrayList<ZipEntry> load(Path filePath, ETConsole console) throws IOException {
-		if(!filePath.toString().split("\\.")[1].equals("pk3")) {
+	public static ArrayList<ZipEntry> load(String filePath, ETConsole console) throws IOException {
+		if(filePath.contains(".") && !filePath.split("\\.")[1].equals("pk3")) {
 			console.println("WARNING: " + filePath + " is an invalid pak3 file.");
 			return null;
 		}
 		
 		ArrayList<ZipEntry> fileList = new ArrayList<ZipEntry>();
-		ZipInputStream stream = new ZipInputStream(new FileInputStream(filePath.toString()));
+		ZipInputStream stream = new ZipInputStream(new FileInputStream(filePath));
 		ZipEntry entry = stream.getNextEntry();
 				
 		while(entry != null) {
